58066fe1678d71f0bff9636ad3a2614da6910b3f
Alvin Law <alvinlaw@gmail.com>
2017-07-19 17:40:43 -0700

    Setup Authentication on Server side.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,96 @@
 const path = require('path');
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const BearerStrategy = require('passport-http-bearer').Strategy;
+
+let secret = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET
+}
+
+if (process.env.NODE_ENV != 'production') {
+  secret = require('./secret');
+}
 
 const app = express();
+app.use(passport.initialize());
 
 // API endpoints go here!
 app.get('/api', (req, res) => {
     res.send('Hello World.');
-} );
+});
+
+// Authentication endpoints
+passport.use(
+    new GoogleStrategy({
+        clientID: secret.CLIENT_ID,
+        clientSecret: secret.CLIENT_SECRET,
+        callbackURL: `/api/auth/google/callback`
+    },
+        (accessToken, refreshToken, profile, cb) => {
+            User
+                .findOneAndUpdate({
+                    googleId: profile.id,
+                    displayName: profile.displayName
+                },
+                {
+                    $set: {
+                        accessToken: accessToken,
+                        googleId: profile.id
+                    }
+                }, {
+                    upsert: true,
+                    new: true
+                })
+                .then((user) => {
+                    return cb(null, user);
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
+        }
+    ));
+
+passport.use(
+    new BearerStrategy(
+        (token, done) => {
+            User
+                .findOne({ accessToken: token })
+                .then((user) => {
+                    if (user) {
+                        return done(null, user);
+                    }
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
+        }
+    )
+);
+
+app.get('/api/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile']
+    }));
+
+app.get('/api/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/',
+        session: false
+    }),
+    (req, res) => {
+        res.cookie('accessToken', req.user.accessToken, { expires: 0 });
+        res.redirect('/');
+    }
+);
+
+app.get('/api/auth/logout', (req, res) => {
+    req.logout();
+    res.clearCookie('accessToken');
+    res.redirect('/');
+});
+
 
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -19,7 +103,7 @@ app.get(/^(?!\/api(\/|$))/, (req, res) => {
 });
 
 let server;
-function runServer(port=3001) {
+function runServer(port = 3001) {
     return new Promise((resolve, reject) => {
         server = app.listen(port, () => {
             resolve();
